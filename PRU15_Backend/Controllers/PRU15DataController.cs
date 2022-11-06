a6b99727c403ca3406d033f87441bfc34f5cbc2d
shamdev-96 <60872045+shamdev-96@users.noreply.github.com>
2022-11-06 12:42:30 +0800

    commit 1

---
+++ PRU15_Backend/Controllers/PRU15DataController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PRU15_Shared;
+
+namespace PRU15_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PRU15DataController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Backend Data for PRU15";
+        }
+
+        [HttpGet]
+        [Route("parlimen")]
+        public List<DataParlimen> GetParlimen(string negeri)
+        {
+            var data = Database.Instance.GetParliament(negeri);
+            return data;
+        }
+
+        [HttpGet]
+        [Route("calon")]
+        public List<DataCalon> GetCalon(string negeri , string kodParlimen)
+        {
+            var data = Database.Instance.GetCalon(negeri , kodParlimen);
+            return data;
+        }
+    }
+}
